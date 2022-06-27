162f2eff5f513f1d638c0ff00a9c6de4e43424da
rust <nrustgen@gmail.com>
2022-06-27 02:57:30 -0700

    5. Очередь на двух стеках [#160 #316144]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<T> {
@@ -7,19 +9,27 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
+    int countIn = 0;
+    int countOut = 0;
+
     public T poll() {
-        if (in == null) {
+        if (countIn == 0) {
             throw new NoSuchElementException();
         }
-        while (out != null) {
-            out.push(in.pop());
-            break;
-        }
 
+        if (countOut == 0) {
+            while (countIn != 0) {
+                out.push(in.pop());
+                countOut++;
+                countIn--;
+            }
+            countOut--;
+        }
         return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
+        countIn++;
     }
 }
