4d312ce15ee31c871d31cf21d9d22ed6898b526b
rust <nrustgen@gmail.com>
2022-07-07 01:49:31 -0700

    0.1. FileOutputStream. [#252488 #319610]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -2,18 +2,24 @@ package ru.job4j.io;
 
 import java.io.FileOutputStream;
 import java.util.Arrays;
+import java.util.List;
 
 public class ResultFile {
     public static void main(String[] args) {
-        int[][] matrix = new int[10][10];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = (i + 1) * (j + 1);
-            }
-        }
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            int[][] matrix = new int[10][10];
             for (int i = 0; i < matrix.length; i++) {
-                out.write(Arrays.deepToString(new int[][]{matrix[i]}).getBytes());
+                for (int j = 0; j < matrix[i].length; j++) {
+                    matrix[i][j] = (i + 1) * (j + 1);
+                }
+            }
+            List<int[]> list = Arrays.stream(matrix).toList();
+            for (int[] val : list) {
+                out.write(Arrays.toString(val)
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace(",", "")
+                        .getBytes());
                 out.write(System.lineSeparator().getBytes());
             }
         } catch (Exception e) {
