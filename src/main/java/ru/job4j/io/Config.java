46eb78508440fc5c8139a60d8e776e5a5130bbf9
rust <nrustgen@gmail.com>
2022-07-08 19:03:00 -0700

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,51 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            for (String val : read.lines().toList()) {
+                if (!(val.contains("#")) && !val.isEmpty()) {
+                    String[] separate = val.split("=", 2);
+                    if (separate.length < 2 || separate[0].isEmpty() || separate[1].isEmpty()) {
+                        throw new IllegalArgumentException();
+                    }
+                    values.put(separate[0], separate[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("./data/app.properties"));
+    }
+}
