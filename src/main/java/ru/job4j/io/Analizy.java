a3ded45274740bc93fda8ba592b7c76bda3ff562
rust <nrustgen@gmail.com>
2022-07-09 03:37:44 -0700

    2. Анализ доступности сервера. [#859 #320237]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -4,41 +4,31 @@ import java.io.*;
 
 public class Analizy {
 
-    public void unavailable(String source, String target) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            boolean server = true;
-            String err = "";
-            for (String value : reader.lines().toList()) {
-                String[] log = value.split(" ");
-                try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-                    if (server && log[0].equals("400") || log[0].equals("500")) {
-                        err = log[0] + ";";
-                        server = false;
-                    }
-                    if (!server && !log[0].equals("400") || !log[0].equals("500")) {
-                        String start = err + log[0] + ";";
-                        out.write(start);
-                        server = true;
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private static boolean server = true;
+
+    public static void unavailable(String source, String target) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            StringBuilder err = new StringBuilder();
+            reader.lines().forEach(str -> check(err, str));
+            out.println(err);
         }
     }
 
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void check(StringBuilder err, String line) {
+        String[] log = line.split(" ");
+        if (server && (log[0].equals("400") || log[0].equals("500"))) {
+            err.append(log[1]).append(";");
+            server = false;
         }
+        if (!server && (log[0].equals("200") || log[0].equals("300"))) {
+            err.append(log[1]).append(";").append(System.lineSeparator());
+            server = true;
+        }
+    }
 
-        Analizy analizy = new Analizy();
-        analizy.unavailable("./data/src/main/java/ru/job4j/io/server.log",
+    public static void main(String[] args) throws IOException {
+        unavailable("./data/server.log",
                 "./data/analizy.txt");
     }
 }
