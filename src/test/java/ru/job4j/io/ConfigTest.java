53f108c52da8fc73f850c24f79f1674c7d4c7198
rust <nrustgen@gmail.com>
2022-07-08 19:01:25 -0700

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+        assertThat(config.value("person"), is("Michael="));
+        assertThat(config.value("fuel"), is("consumption=34=mpg"));
+    }
+
+    @Test
+    public void whenPairContainsCommentAndEmptyLines() {
+        String path = "./data/app1.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Boris"));
+        assertThat(config.value("person"), is("Frenchie="));
+        assertThat(config.value("surname="), is(Matchers.nullValue()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutKey() {
+        String path = "./data/appEx.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutValue() {
+        String path = "./data/appEx1.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutKeyAndValue() {
+        String path = "./data/appEx2.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutSplitSign() {
+        String path = "./data/appEx3.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+
+}
