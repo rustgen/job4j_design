05efd09283b19b40a47818084ef04cddd08b03b5
rust <nrustgen@gmail.com>
2022-07-13 12:48:48 -0700

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -32,7 +32,7 @@ public class SearchFiles implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (condition.test(file)) {
-            paths.add(file.toAbsolutePath());
+            paths.add(file);
         }
         return CONTINUE;
     }
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -9,20 +9,13 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<Path> sources, File target) throws IOException {
-        ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)));
-        for (Path path : sources) {
-            zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
-            BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()));
-            zip.write(out.readAllBytes());
-        }
-    }
-
-    public void packSingleFile(File source, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                    zip.write(out.readAllBytes());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -44,15 +37,10 @@ public class Zip {
 
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
         ArgsName map = zipValidate(args);
         List<Path> list = Search.search(Paths.get(map.get("d")),
                 path -> !path.toFile().getName().endsWith(map.get("e")));
-        Zip zipBunch = new Zip();
-        zipBunch.packFiles(list, new File(map.get("o")));
+        zip.packFiles(list, new File(map.get("o")));
     }
 }
 
