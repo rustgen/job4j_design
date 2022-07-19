6d37c614ed65615dde032f5175a84425f15dcb9f
rust <nrustgen@gmail.com>
2022-07-19 03:34:52 -0700

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Tony";
+        byte age = 48;
+        short month = 6;
+        int year = 1973;
+        char gender = 'M';
+        long number = 3234437586L;
+        float height = 5.7f;
+        double weight = 124.7;
+        boolean exist = false;
+        LOG.debug("User info name : {}, age : {}, month : {}, year : {}, gender : {}, number : {}, "
+                + "height : {}, weight : {}, exist : {}.", name, age, month, year, gender,
+                number, height, weight, exist);
     }
 }
