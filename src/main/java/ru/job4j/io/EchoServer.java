4e7414e285b8a70e160515dc598286669eff6681
rust <nrustgen@gmail.com>
2022-07-19 02:45:15 -0700

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,8 +14,13 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (in.readLine().contains("?msg=Bye")) {
+                    String read = in.readLine();
+                    if (read.contains("?msg=Exit")) {
                         server.close();
+                    } else if (read.contains("?msg=Hello")) {
+                        out.write("Hello!\r\n\r\n".getBytes());
+                    } else if (read.contains("??msg=What")) {
+                        out.write("What?\r\n\r\n".getBytes());
                     }
                     out.flush();
                 }
