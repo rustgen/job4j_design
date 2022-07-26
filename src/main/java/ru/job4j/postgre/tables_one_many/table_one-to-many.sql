d8d2cfe3d1cdc7a2067d54faff3defca4dc8fb74
rust <nrustgen@gmail.com>
2022-07-26 10:51:04 -0700

    0.1. Связи между таблицами [#373615 #325938]

--- src/main/java/ru/job4j/postgre/tables_one_many/table_one-to-many.sql
+++ src/main/java/ru/job4j/postgre/tables_one_many/table_one-to-many.sql
@@ -1,17 +1,17 @@
-create table powerbank(
-    id serial primary key,
-    number text
-);
-
 create table people(
     id serial primary key,
-    name varchar(255),
-    powerbank_id references powerbank(id)
+    name varchar(255)
 );
 
-insert into powerbank(number) values ('office #2');
-insert into people(name, powerbank_id) values ('Jim', 1);
+create table powerbank(
+    id serial primary key,
+    number text,
+    people_id references people(id)
+);
 
-select * from people;
+insert into people(name, powerbank_id) values ('Jim');
+insert into powerbank(number, people_id) values ('office #2', 1);
 
-select * from powerbank where id in (select id from people);
+select * from powerbank;
+
+select * from people where id in (select id from pewerbank_id);
