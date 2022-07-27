fcb2060874943d45518932959d829c4c0ecbcab9
rust <nrustgen@gmail.com>
2022-07-27 14:15:16 -0700

    1. Схема прав пользователи и роли. [#1093 #326246]

--- src/main/java/ru/job4j/postgre/schemes_of_users/create.sql
+++ src/main/java/ru/job4j/postgre/schemes_of_users/create.sql
@@ -15,18 +15,20 @@ create table rules(
     name varchar(255)
 );
 
+create table category(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table state(
+    id serial primary key,
+    name varchar(255)
+);
+
 create table role_rules(
     id serial primary key,
     role_id int references role(id),
     rules_id int references rules(id)
-);
-
-create table item(
-    id serial primary key,
-    name varchar(255),
-    users_id int references users(id),
-    category_id int references category(id),
-    state_id int references state(id)
 );
 
 create table comments(
@@ -41,12 +43,10 @@ create table attachs(
     item_id int references item(id)
 );
 
-create table category(
+create table item(
     id serial primary key,
-    name varchar(255)
-);
-
-create table state(
-    id serial primary key,
-    name varchar(255)
+    name varchar(255),
+    users_id int references users(id),
+    category_id int references category(id),
+    state_id int references state(id)
 );
