e06c0fc224404204e1c09521e4006824143e4e57
rust <nrustgen@gmail.com>
2022-07-31 10:39:41 -0700

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,33 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+
+    File file = new File("");
+    public void readDriver() throws IOException {
+
+    }
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config("./data/app.properties");
+        config.load();
+
+        Class.forName(config.value("hibernate.connection.driver_class"));
+        String url = config.value("hibernate.connection.url");
+        String login = config.value("hibernate.connection.username");
+        String password = config.value("hibernate.connection.password");
+
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
